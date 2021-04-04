4e5d909ca0f593e943a381309010c46ebc256bf8
Daniel Fernandez <39488892+dferndz@users.noreply.github.com>
2021-04-04 17:37:07 -0500

    updated homepage and license

--- cpp-utils.rb
+++ cpp-utils.rb
@@ -3,10 +3,10 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class CppUtils < Formula
   desc ""
-  homepage ""
+  homepage "https://github.com/dferndz/cpp-utils"
   url "https://github.com/dferndz/cpp-utils/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "05e26e87ed5892d3aa232307547a090463efd7d65e69d6c53a49f5bdacc29103"
-  license ""
+  license "MIT"
 
   # depends_on "cmake" => :build
 
