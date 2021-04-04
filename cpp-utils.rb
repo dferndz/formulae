2a746174a87a5ddffb788d7468e25f351e8d5cfa
Daniel Fernandez <39488892+dferndz@users.noreply.github.com>
2021-04-04 17:05:43 -0500

    initial

---
+++ cpp-utils.rb
@@ -0,0 +1,16 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class CppUtils < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/dferndz/cpp-utils/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "05e26e87ed5892d3aa232307547a090463efd7d65e69d6c53a49f5bdacc29103"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    include.install "utils.h"
+  end
+end
