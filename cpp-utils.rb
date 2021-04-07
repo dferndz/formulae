6227faf0ad22c990553fedd36599fdbf06c310c2
Daniel Fernandez <39488892+dferndz@users.noreply.github.com>
2021-04-06 22:30:14 -0500

    fix sha

--- cpp-utils.rb
+++ cpp-utils.rb
@@ -5,7 +5,7 @@ class CppUtils < Formula
   desc ""
   homepage "https://github.com/dferndz/cpp-utils"
   url "https://github.com/dferndz/cpp-utils/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "7cb8d3442d12918e19513974ecb05783457d9f115804634ca7f422204cbf738a"
+  sha256 "9014203d71e9f50aaae21999a043f3e68cc2bc5b0179ea3ea8977986ae6a14a4"
   license "MIT"
 
   # depends_on "cmake" => :build
